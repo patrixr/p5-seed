f39c675ac79023e788acbf6363ff940efa29d480
Patrick R <1822532+patrixr@users.noreply.github.com>
2021-10-02 13:39:04 +0800

    :label: UPDATE: don't include global p5 types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="vite/client" />
-/// <reference path="../node_modules/@types/p5/global.d.ts" />
+/// <reference path="../node_modules/@types/p5/index.d.ts" />
 
 declare module 'stats-js';
